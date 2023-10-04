0f94fa7a757f8b400ac2ed1b36c726280bbdf448
Cristian <cristian@voixdigitale.com>
2023-10-04 19:57:04 +0200

    Added some sound and color

--- Labyrinthe/main.cs
+++ Labyrinthe/main.cs
@@ -1,10 +1,17 @@
-﻿using System.Numerics;
+﻿using System.Drawing;
+using System.Numerics;
 using System.Runtime.InteropServices;
+using static System.Net.Mime.MediaTypeNames;
 
 ConsoleKeyInfo cki;
+string cell;
+
 int sizeX = 10;
 int sizeY = 10;
+int keys = 0;
 int[] playerPos = new int[] { 0, 0 };
+bool gameFinished = false;
+
 string[,] labyrinthe = new string[,] { //Attention, pour l'accéder il faut faire labyrinthe[y, x]
     { "joueur", "vide", "║║║", "vide",  "vide", "vide", "vide", "vide", "║║║",  "clé" },
     { "vide",   "═══",  "╩═╝", "vide",  "╔═╦",  "═══",  "╦═╗", "vide",  "╚═╝",  "vide" },
@@ -17,15 +24,16 @@ string[,] labyrinthe = new string[,] { //Attention, pour l'accéder il faut fair
     { "vide",   "╚═╝",  "vide", "╚═╝",  "clé",  "║║╠",  "╩═╝", "vide",  "═══",  "═══" },
     { "vide",   "vide", "vide", "vide", "vide", "╚═╝", "vide", "vide",  "vide", "vide" }
 };
-string cell;
-int keys = 0;
+
+ConsoleColor color = Console.ForegroundColor;
+ConsoleColor bgColor = Console.BackgroundColor;
 
 Draw();
 
 do {
     cki = Console.ReadKey();
     HandleInput(cki.Key);
-} while (cki.Key != ConsoleKey.Escape);
+} while (cki.Key != ConsoleKey.Escape || gameFinished);
 
 
 void HandleInput(ConsoleKey key) {
@@ -41,8 +49,14 @@ void HandleInput(ConsoleKey key) {
     UpdatePlayerPos();
 }
 
-void UpdateCell(int x, int y, string cell, bool isUI = false) {
+void UpdateCell(int x, int y, string cell, ConsoleColor color, ConsoleColor bgColor, bool isUI = false) {
     (int cursorLeft, int cursorTop) = Console.GetCursorPosition();
+    ConsoleColor originalBG = Console.BackgroundColor;
+    ConsoleColor originalFG = Console.ForegroundColor;
+
+    Console.BackgroundColor = bgColor;
+    Console.ForegroundColor = color;
+    
     Console.SetCursorPosition(x*3, y);
     string cellContent = cell switch {
         "joueur" => " █ ",
@@ -54,6 +68,9 @@ void UpdateCell(int x, int y, string cell, bool isUI = false) {
     Console.SetCursorPosition(cursorLeft, cursorTop);
 
     if (!isUI) labyrinthe[y, x] = cell;
+
+    Console.BackgroundColor = originalBG;
+    Console.ForegroundColor = originalFG;
 }
 
 void MoveDown() {
@@ -66,8 +83,8 @@ void MoveDown() {
                 AddKey();
             }
 
-            UpdateCell(x, y, "vide");
-            UpdateCell(x, y + 1, "joueur");
+            UpdateCell(x, y, "vide", color, bgColor);
+            UpdateCell(x, y + 1, "joueur", color, bgColor);
             playerPos[1]++;
         }
     }
@@ -83,8 +100,8 @@ void MoveUp() {
                 AddKey();
             }
 
-            UpdateCell(x, y, "vide");
-            UpdateCell(x, y - 1, "joueur");
+            UpdateCell(x, y, "vide", color, bgColor);
+            UpdateCell(x, y - 1, "joueur", color, bgColor);
             playerPos[1]--;
         }
     }
@@ -100,8 +117,8 @@ void MoveLeft() {
                 AddKey();
             }
 
-            UpdateCell(x, y, "vide");
-            UpdateCell(x - 1, y, "joueur");
+            UpdateCell(x, y, "vide", color, bgColor);
+            UpdateCell(x - 1, y, "joueur", color, bgColor);
             playerPos[0]--;
         }
     }
@@ -117,17 +134,34 @@ void MoveRight() {
                 AddKey();
             }
 
-            UpdateCell(x, y, "vide");
-            UpdateCell(x + 1, y, "joueur");
+            UpdateCell(x, y, "vide", color, bgColor);
+            UpdateCell(x + 1, y, "joueur", color, bgColor);
             playerPos[0]++;
         }
+    } else if (x == sizeX - 1 && y == sizeY - 1 && keys == 4) { //Victory
+        UpdateCell(x, y, "vide", color, bgColor);
+        UpdateCell(x + 2, y, "joueur", color, bgColor, true);
+        ReplaceInputMessage();
+        PlayVictorySong();
+        Environment.Exit(0);
     }
 }
 
 void AddKey() {
     keys++;
-    UpdateCell(sizeX + 1, 1, $"Clés : {keys}", true);
-    Console.Beep();
+    UpdateCell(sizeX + 1, 1, $"Clés : {keys}", color, bgColor, true);
+    PlayGetKeySound();
+
+    if (keys == 4) {
+        OpenExit();    
+    }
+}
+
+void OpenExit() {
+    UpdateCell(sizeX, 9, "▓▓", ConsoleColor.Yellow, ConsoleColor.Black, true);
+    PlayOpenDoorSound();
+    
+    UpdateCell(sizeX, 9, "     -> Exit", color, bgColor, true);
 }
 
 Boolean canStepOn(string cellType) {
@@ -145,20 +179,77 @@ void Draw() {
                 "clé" => " ⌐ ",
                 _ => labyrinthe[i, j]
             };
+
+            if (labyrinthe[i, j] == "clé") {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+
             Console.Write(cell);
 
             if (j == sizeX - 1) {
-                Console.Write("░░\n");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("▓▓\n");
             }
+
+            Console.ForegroundColor = color;
         }
     }
     for (int i = 0; i < sizeX * 3 + 2; i++) {
-        Console.Write("░");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("▓");
     }
+    Console.ForegroundColor = color;
+
+    UpdateCell(sizeX, 9, "▓▓", ConsoleColor.DarkRed, ConsoleColor.Black, true);
     Console.WriteLine($"\n\nPosition joueur : {playerPos[0]}, {playerPos[1]}");
     Console.WriteLine("\nUtilise les flèches du clavier pour bouger, ESC pour sortir :");
 }
 
 void UpdatePlayerPos() {
-    UpdateCell(0, 12, $"Position joueur : {playerPos[0]}, {playerPos[1]}", true);
+    UpdateCell(0, 12, $"Position joueur : {playerPos[0]}, {playerPos[1]}", color, bgColor, true);
+}
+
+void ReplaceInputMessage() {
+    UpdateCell(0, 14, "Félicitations et merci d'avoir joué !                       ", ConsoleColor.Magenta, bgColor, true);
+}
+
+void PlayGetKeySound() {
+    Console.Beep(800, 25);
+    Console.Beep(1400, 100);
+}
+
+void PlayOpenDoorSound() {
+    for (int i = 0; i < 15; i++) {
+        Console.Beep(300, 25);
+    }
+}
+
+void PlayVictorySong() {
+    Console.Beep(130, 100);
+    Console.Beep(262, 100);
+    Console.Beep(330, 100);
+    Console.Beep(392, 100);
+    Console.Beep(523, 100);
+    Console.Beep(660, 100);
+    Console.Beep(784, 300);
+    Console.Beep(660, 300);
+    Console.Beep(146, 100);
+    Console.Beep(262, 100);
+    Console.Beep(311, 100);
+    Console.Beep(415, 100);
+    Console.Beep(523, 100);
+    Console.Beep(622, 100);
+    Console.Beep(831, 300);
+    Console.Beep(622, 300);
+    Console.Beep(155, 100);
+    Console.Beep(294, 100);
+    Console.Beep(349, 100);
+    Console.Beep(466, 100);
+    Console.Beep(588, 100);
+    Console.Beep(699, 100);
+    Console.Beep(933, 300);
+    Console.Beep(933, 100);
+    Console.Beep(933, 100);
+    Console.Beep(933, 100);
+    Console.Beep(1047, 400);
 }
